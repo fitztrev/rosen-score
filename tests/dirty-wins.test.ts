b1b24d82e1223f20170ac0deba60c38e4a5c1eb6
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-01-31 19:30:50 -0500

    increase code coverage

--- tests/dirty-wins.test.ts
+++ tests/dirty-wins.test.ts
@@ -7,10 +7,26 @@ describe('clutch pawn', () => {
         [
             // https://lichess.org/tgMQgOSk#149
             '1r6/4K3/P7/8/2q5/3qk3/8/8 b - - 2 75',
+            'white',
             [{ color: 'w' }],
         ],
-    ])('test fen: %p', (fen, expected) => {
-        expect(clutchPawn({ result: { winner: 'white', via: 'timeout' } } as Game, fen)).toStrictEqual(expected)
+        ['1R6/4k3/p7/8/2Q5/3QK3/8/8 w - - 2 75', 'black', [{ color: 'b' }]],
+    ])('test fen: %p', (fen, winner, expected) => {
+        expect(clutchPawn({ result: { winner, via: 'timeout' } } as Game, fen)).toStrictEqual(expected)
+    })
+})
+
+describe('not clutch pawn', () => {
+    test.each([
+        // white has 2 pawns
+        '1r6/4K3/P6P/8/2q5/3qk3/8/8 b - - 2 75',
+
+        // black has 2 pawns
+        '1R6/4k3/p6p/8/2Q5/3QK3/8/8 w - - 2 75',
+    ])('test fen: %p', (fen) => {
+        expect(clutchPawn({ result: { winner: 'white', via: 'timeout' } } as Game, fen)).toStrictEqual([])
+        expect(clutchPawn({ result: { winner: 'black', via: 'timeout' } } as Game, fen)).toStrictEqual([])
+        expect(clutchPawn({ result: { outcome: 'draw' } } as Game, fen)).toStrictEqual([])
     })
 })
 
@@ -23,6 +39,7 @@ describe('insufficient material', () => {
         ],
     ])('test fen: %p', (fen, expected) => {
         expect(winInsufficientMaterial({ result: { winner: 'white', via: 'timeout' } } as Game, fen)).toStrictEqual(expected)
+        expect(winInsufficientMaterial({ result: { outcome: 'draw' } } as Game, fen)).toStrictEqual([])
     })
 })
 
--- tests/double-check-checkmate.test.ts
+++ tests/double-check-checkmate.test.ts
@@ -18,3 +18,18 @@ describe('test double-check checkmate', () => {
         expect(doubleCheckCheckmate({ result: { winner: 'white', via: 'checkmate' }, moves: [{}, {}, {}] } as Game, fen)).toStrictEqual(expected)
     })
 })
+
+describe('test not double-check checkmate', () => {
+    test.each([
+        [
+            // king not in check
+            'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+
+            // king in check by 1 piece
+            'rnbqkbnr/ppppp1pp/8/5p1Q/8/4P3/PPPP1PPP/RNB1KBNR b KQkq - 0 1',
+        ],
+    ])('test fen: %p', (fen) => {
+        expect(doubleCheckCheckmate({ result: { outcome: 'draw' }, moves: [{}, {}, {}] } as Game, fen)).toStrictEqual([])
+        expect(doubleCheckCheckmate({ result: { winner: 'white', via: 'checkmate' }, moves: [{}, {}, {}] } as Game, fen)).toStrictEqual([])
+    })
+})
