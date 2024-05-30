322e4486957b9734cf9998be015bfca8ebdd16e5
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-05-29 22:21:43 -0400

    update material diff calculation, queen worth 9

--- js/utils/calculate-material-imbalance.ts
+++ js/utils/calculate-material-imbalance.ts
@@ -3,11 +3,11 @@ export function calculateMaterialImbalance(position: string): number {
         if (piece === 'p') return -1
         else if (piece === 'n' || piece === 'b') return -3
         else if (piece === 'r') return -5
-        else if (piece === 'q') return -8
+        else if (piece === 'q') return -9
         else if (piece === 'P') return +1
         else if (piece === 'N' || piece === 'B') return +3
         else if (piece === 'R') return +5
-        else if (piece === 'Q') return +8
+        else if (piece === 'Q') return +9
         else return 0
     })
 
--- tests/calculate-material-imbalances.test.ts
+++ tests/calculate-material-imbalances.test.ts
@@ -6,9 +6,9 @@ import { fenToPosition } from '../js/utils/fen-to-position'
 describe('test material imbalance calculations', () => {
     test.each([
         ['rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', 0],
-        ['rbnq4/p7/8/8/8/8/8/8 w - - 0 1', -20],
-        ['8/8/8/8/8/8/P7/RBNQ4 w - - 0 1', 20],
-        ['rb6/8/8/8/8/8/P7/2NQ4 w - - 0 1', 4],
+        ['rbnq4/p7/8/8/8/8/8/8 w - - 0 1', -21],
+        ['8/8/8/8/8/8/P7/RBNQ4 w - - 0 1', 21],
+        ['rb6/8/8/8/8/8/P7/2NQ4 w - - 0 1', 5],
     ])('test FEN: %p', (fen, value) => {
         expect(calculateMaterialImbalance(fenToPosition(fen))).toEqual(value)
     })
